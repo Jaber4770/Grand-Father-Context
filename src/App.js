242252278625be04aa4745061ef87760da750bed
jaber4770 <jaber.ahmed4770@gmail.com>
2022-01-16 11:37:06 +0600

    father and uncle added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import GrandFather from './component/GrandFather/GrandFather';
+import Father from './component/Father/Father';
+import Uncle from './component/Uncle/Uncle';
 
 function App() {
   return (
     <div className="App">
-      
+      <GrandFather></GrandFather>
+      <Father></Father>
+      <Uncle></Uncle>
     </div>
   );
 }
